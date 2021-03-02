d8f8657543d28ac1071ad3511c0084e8662d7fad
Silvin Pradhan <silvinpradhan95@gmail.com>
2021-03-02 14:50:12 -0600

    Resolved Port entries with dotenv package

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('express');
+require('dotenv').config();
+
+// APP
 const app = express();
-const PORT = 3000;
-app.listen(PORT, () => console.log('Server listening in port 3000!'));
+
+// Middleware
+
+// Routes
+
+// Execute
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+	console.log(`Server listening at http://localhost:${port}`);
+});
