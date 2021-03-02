29038ad788c9153423bd97bb69fcbb3472dc433f
Silvin Pradhan <silvinpradhan95@gmail.com>
2021-03-02 16:31:51 -0600

    added the bodyParser, cors middleware, checks if the environemtn is development or production and exectutes express server

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 const express = require('express');
+const path = require('path'); // allows to dynamically build when we call it from current directory to where we actually go
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('express');
-require('dotenv').config();
+
+if (process.env.NODE_ENV === 'development') require('dotenv').config();
 
 // APP
 const app = express();
 
 // Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, 'client/build')));
+
+	app.get('*', function (req, res) {
+		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+	});
+}
 // Routes
 
 // Execute
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+app.listen(port, (error) => {
+	if (error) throw error;
 	console.log(`Server listening at http://localhost:${port}`);
 });
